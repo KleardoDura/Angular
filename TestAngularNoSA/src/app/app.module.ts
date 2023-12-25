a0d453801eac65e40f9844e15e01e8801b1ae41d
KLEARDODURA\klear <kleardodura13@gmail.com>
2023-12-25 21:11:48 +0100

    no message

--- TestAngularNoSA/src/app/app.module.ts
+++ TestAngularNoSA/src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ProductComponent } from './product/product.component';
 import {HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms'; //Getting data from input 
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component'; //Getting data from input 
+
+const appRoute: Routes=[
+  {path:'menu',component:MenuComponent},
+  {path:'',component:NavComponent},
+  {path:'**',component:ErrorComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    ProductComponent
+    ProductComponent,
+    MenuComponent,
+    ErrorComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoute), //per routes
     FormsModule, //Getting data from input 
     HttpClientModule,
     BrowserModule,
